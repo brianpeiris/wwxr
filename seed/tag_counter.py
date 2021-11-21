2bae49acd6941467707fd578d820e68e5a1057a4
Brian Peiris <brianpeiris@gmail.com>
2021-11-20 23:46:54 -0500

    commoncrawl video finder

--- seed/tag_counter.py
+++ seed/tag_counter.py
@@ -1,41 +1,35 @@
 import re
 from collections import Counter
 from mrcc import CCJob
+import logging
+import pymongo
 
+client = pymongo.MongoClient('mongodb://db:27017')
+db = client.wwxr
+pages = db.pages
 
-def get_tag_count(data, ctr=None):
-    """Extract the names and total usage count of all the opening HTML tags in the document"""
-    if ctr is None:
-        ctr = Counter()
-    # Convert the document to lower case as HTML tags are case insensitive
-    ctr.update(HTML_TAG_PATTERN.findall(data.lower()))
-    return ctr
+LOG = logging.getLogger('CCJob')
 
-
-# Optimization: compile the regular expression once so it's not done each time
-# The regular expression looks for (1) a tag name using letters (assumes lowercased input) and numbers
-# and (2) allows an optional for a space and then extra parameters, eventually ended by a closing >
-HTML_TAG_PATTERN = re.compile(b'<([a-z0-9]+)[^>]*>')
-# Let's check to make sure the tag counter works as expected
-assert get_tag_count(b'<html><a href="..."></a><h1 /><br/><p><p></p></p>') == {b'html': 1, b'a': 1, b'p': 2, b'h1': 1, b'br': 1}
-
+VIDEO_TAG_PATTERN = re.compile(b'<video[^>]*>')
 
 class TagCounter(CCJob):
     def process_record(self, record):
-        # WARC records have three different types:
-        #  ["application/warc-fields", "application/http; msgtype=request", "application/http; msgtype=response"]
-        # We're only interested in the HTTP responses
         if record['Content-Type'] == 'application/http; msgtype=response':
             payload = record.payload.read()
-            # The HTTP response is defined by a specification: first part is headers (metadata)
-            # and then following two CRLFs (newlines) has the data for the response
             headers, body = payload.split(b'\r\n\r\n', 1)
             if b'Content-Type: text/html' in headers:
-                # We avoid creating a new Counter for each page as that's actually quite slow
-                tag_count = get_tag_count(body)
-                for tag, count in tag_count.items():
-                    yield tag.decode('ascii'), count
+                if VIDEO_TAG_PATTERN.search(body) is not None:
+                    url = record.url
+                    pages.insert_one({'url': url})
+                    self.increment_counter('commoncrawl', 'videos_found', 1)
+                    yield None, None
                 self.increment_counter('commoncrawl', 'processed_pages', 1)
+
+    def reducer(self, keys, values):
+        yield None, None
+
+    def reducer_final(self):
+        client.close()
 
 
 if __name__ == '__main__':
--- site/src/cli.js
+++ site/src/cli.js
@@ -2,9 +2,19 @@ const { connectMongo } = require("./utils");
 
 (async () => {
   const { client, db } = await connectMongo();
+
   const pages = db.collection("pages");
-  const title = process.argv[2];
-  await pages.insertOne({ title });
-  console.log(`inserted ${title}`);
+
+  const command = process.argv[2];
+
+  if (command === "insert") {
+    const title = process.argv[3];
+    await pages.insertOne({ title });
+    console.log(`inserted ${title}`);
+  } else if (command === "delete_all") {
+    const result = await pages.deleteMany({});
+    console.log(`deleted ${result.deletedCount}`)
+  }
+
   client.close();
 })();
